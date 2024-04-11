7d64bb046b3baa7f6974349e8a15cdfc0dd8098b
Ali <ali.arslan.93@hotmail.com>
2024-04-11 06:30:13 +0200

    add application interface and initialize app

--- app/Repositories/Interfaces/RoyalAppInterface.php
+++ app/Repositories/Interfaces/RoyalAppInterface.php
@@ -4,5 +4,9 @@ namespace App\Repositories\Interfaces;
 
 interface RoyalAppInterface
 {
-
+    /**
+     * 
+     * app gonna have to base url and auth token for client initialize.
+     */
+    public function initializeClient(): void;
 }
